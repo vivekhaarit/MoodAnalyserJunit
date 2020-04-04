51566c7536b02dc47be4d6fadd009dfe8040307b
vivek kumar <vivekhaarit@gmail.com>
2020-04-04 12:25:52 +0530

    added ObjectReflector file

--- src/test/java/com/bridgelabz/junit/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/junit/MoodAnalyserTest.java
@@ -4,7 +4,34 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class MoodAnalyserTest {
+
+    @Test
+    public void givenMoodAnalyserClass_ShouldReturnObject() throws MoodAnalyserException {
+
+        Constructor<?> constructor = null;
+        try {
+            constructor = Class.forName("com.bridgelabz.junit.MooodAnalyser").getConstructor(String.class);
+            Object mood = (MooodAnalyser)constructor.newInstance("I'm in HAPPY Mood");
+            MooodAnalyser moodAnalyser = (MooodAnalyser) mood;
+            String analyseMood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY",analyseMood);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void MoodTest() throws MoodAnalyserException {
         MooodAnalyser mooodAnalyser = new MooodAnalyser("I'm in SAD mood");
